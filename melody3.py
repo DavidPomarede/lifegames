046c5b596a2e888f79480d00a4063b4de07f70f4
David Pom <davidpom@gmail.com>
2023-03-01 11:38:20 -0500

    more_melodies2

--- melody3.py
+++ melody3.py
@@ -2,17 +2,17 @@
 
 from scamp import *
 
-s = Session ( )
+s = Session()
 
-s. tempo = 120
+s.tempo = 120
 
-violin = s.new part("violin" )
+violin = s.new_part("violin" )
 
 # pitches and durations from the opening of the Sibelius violin concerto
 
-pitches = [79, 81, 74, , 74, 77, 76, 77, 76, 74, 81, 84, 83, 81, 80, 81, 86, 88, 84, 83, 81, 80, 81, 81]
+pitches = [79, 81, 74, 73, 74, 77, 76, 77, 76, 74, 81, 84, 83, 81, 80, 81, 86, 88, 84, 83, 81, 80, 81, 81]
 
 durations = [3.5, 0.5, 5, 1, 1.5, 0.5/3, 0.5/3, 0.5/3, 5, 1, 1.5, 0.5, 1.5, 0.25, 0.25, 3, 1, 1.5, 0.5, 1.5, 0.25, 0.25, 4]
 
 for pitch, dur in zip(pitches, durations) :
-	violin.play_note(pitch, 0.75, dur)
+    violin.play_note(pitch, 0.75, dur)
